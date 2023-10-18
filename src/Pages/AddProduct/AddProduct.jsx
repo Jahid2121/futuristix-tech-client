81e1ddf8ab1f2dfa831f04adf7b9cbb757ea7914
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-10-18 22:32:52 +0600

    ADD Product page Updated

--- src/Pages/AddProduct/AddProduct.jsx
+++ src/Pages/AddProduct/AddProduct.jsx
@@ -1,102 +1,131 @@
 import React from "react";
 
 const AddProduct = () => {
+  const handleAddProduct = e => {
+    e.preventDefault()
+    const form = e.target 
+    const productName = form.productName.value 
+    const brandName = form.brandName.value
+    const type = form.type.value
+    const price = form.price.value
+    const description = form.description.value
+    const ratings = form.ratings.value
+    const image = form.image.value
+    const newProduct = {productName, brandName, type, price, description, ratings, image}
+    console.log(newProduct);
+  }
+
   return (
-    <div className="bg-[#F4F3F0]">
-      <h2 className="text-center text-3xl pt-24 font-bold">Add a Product</h2>
-      {/* Name and Brand Name Row */}
-      <div className="md:flex gap-6 p-4 px-8">
-      <div className="form-control  md:w-1/2">
-        <label className="label">
-          <span className="label-text">Product Name</span>
-        </label>
-        <label className="input-group">
-          <input
-            type="text"
-            placeholder="Product Name"
-            className="input w-full  input-bordered"
-          />
-        </label>
-      </div>
-      <div className="form-control md:w-1/2">
-        <label className="label">
-          <span className="label-text">Brand Name</span>
-        </label>
-        <label className="input-group">
-          <input
-            type="text"
-            placeholder="e.g: Apple"
-            className="input w-full input-bordered"
-          />
-        </label>
-      </div>
-    </div>
-      <div className="md:flex gap-6 p-4 px-8">
-      <div className="form-control  md:w-1/2">
-        <label className="label">
-          <span className="label-text">Type</span>
-        </label>
-        <label className="input-group">
-          <input
-            type="text"
-            placeholder="e.g: phone, computer, headphone"
-            className="input w-full  input-bordered"
-          />
-        </label>
-      </div>
-      <div className="form-control md:w-1/2">
-        <label className="label">
-          <span className="label-text">Price</span>
-        </label>
-        <label className="input-group">
-          <input
-            type="text"
-            placeholder="Price in dollar"
-            className="input w-full input-bordered"
-          />
-        </label>
-      </div>
-    </div>
-      <div className="md:flex gap-6 p-4 px-8">
-      <div className="form-control  md:w-1/2">
-        <label className="label">
-          <span className="label-text">Short description</span>
-        </label>
-        <label className="input-group">
-          <input
-            type="text"
-            placeholder="Short description..."
-            className="input w-full  input-bordered"
-          />
-        </label>
-      </div>
-      <div className="form-control md:w-1/2">
-        <label className="label">
-          <span className="label-text">Rating</span>
-        </label>
-        <label className="input-group">
-          <input
-            type="text"
-            placeholder="Rating(numbers only)"
-            className="input w-full input-bordered"
-          />
-        </label>
-      </div>
-      
-    </div>
-    <div className="form-control mx-auto md:w-1/2">
-        <label className="label">
-          <span className="label-text text-center  mx-auto">Image URL</span>
-        </label>
-        <label className="input-group">
-          <input
-            type="text"
-            placeholder="e.g: https://i.ibb.co/..."
-            className="input w-full input-bordered"
-          />
-        </label>
-      </div>
-      <button className="text-center bg-slate-400 px-16 flex mx-auto btn my-3">Add Product</button>
+    <div style={{backgroundImage: "url('https://i.ibb.co/khyLmT4/background-2426328.jpg')"}} className="bg-cover pb-8">
+      <h2 className="text-center text-3xl pt-24 text-white font-bold">Add a Product</h2>
+      <form onSubmit={handleAddProduct}>
+        {/* Name and Brand Name Row */}
+        <div className="md:flex gap-6 p-4 px-8">
+          <div className="form-control  md:w-1/2">
+            <label className="label">
+              <span className="label-text text-white">Product Name</span>
+            </label>
+            <label className="input-group">
+              <input
+              name="productName"
+                type="text"
+                placeholder="Product Name"
+                className="input w-full  input-bordered"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text text-white">Brand Name</span>
+            </label>
+            <label className="input-group">
+              <input
+              name="brandName"
+                type="text"
+                placeholder="e.g: Apple"
+                className="input w-full input-bordered"
+              />
+            </label>
+          </div>
+        </div>
+        {/* Type and Price Row */}
+        <div className="md:flex gap-6 p-4 px-8">
+          <div className="form-control  md:w-1/2">
+            <label className="label">
+              <span className="label-text text-white">Type</span>
+            </label>
+            <label className="input-group">
+              <input
+              name="type"
+                type="text"
+                placeholder="e.g: phone, computer, headphone"
+                className="input w-full  input-bordered"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text text-white">Price</span>
+            </label>
+            <label className="input-group">
+              <input
+              name="price"
+                type="text"
+                placeholder="Price in dollar"
+                className="input w-full input-bordered"
+              />
+            </label>
+          </div>
+        </div>
+        {/* Description and Rating Row */}
+        <div className="md:flex gap-6 p-4 px-8">
+          <div className="form-control  md:w-1/2">
+            <label className="label">
+              <span className="label-text text-white">Short description</span>
+            </label>
+            <label className="input-group">
+              <input
+              name="description"
+                type="text"
+                placeholder="Short description..."
+                className="input w-full  input-bordered"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text text-white">Ratings</span>
+            </label>
+            <label className="input-group">
+              <input
+              name="ratings"
+                type="text"
+                placeholder="Ratings(numbers only)"
+                className="input w-full input-bordered"
+              />
+            </label>
+          </div>
+        </div>
+        {/* Image Url Row */}
+        <div className="form-control mx-auto md:w-1/2">
+          <label className="label">
+            <span className="label-text text-white text-center  mx-auto">Image URL</span>
+          </label>
+          <label>
+            <input
+            name="image"
+              type="text"
+              placeholder="e.g: https://i.ibb.co/..."
+              className="input w-full input-bordered"
+            />
+          </label>
+        </div>
+        <input
+          type="submit"
+          value="Add Product"
+          className="text-center bg-slate-400 px-16 btn-block  flex mx-auto btn my-3"
+        />
+      </form>
     </div>
   );
 };
