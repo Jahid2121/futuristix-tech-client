6f409ec40e535d1849576c6da9036b874b64e5ae
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-10-18 21:11:03 +0600

    Update Sign Up page

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -41,47 +41,50 @@ const SignUp = () => {
     })
   }
   return (
-    <div className="bg-cover  bg-center min-h-screen glass bg-transparent" style={{backgroundImage: "url('https://i.ibb.co/cvTpZrk/fabio-oy-Xis2k-ALVg-unsplash.jpg')"}}>
+    <div className="bg-cover   min-h-screen  " style={{backgroundImage: "url('https://i.ibb.co/cvTpZrk/fabio-oy-Xis2k-ALVg-unsplash.jpg')"}}>
+      <h2 className="text-white text-3xl font-bold text-center mb-9">Welcome</h2>
 
-      <form  onSubmit={handleLogin} className="card-body border-2 w-[400px] mx-auto ">
-        <div className="form-control">
+      <form style={{backdropFilter: 'blur(3px)'}}  onSubmit={handleLogin} className="card-body rounded-xl  border-2 w-[400px] mx-auto ">
+        <h2 className="text-white text-3xl font-bold   text-center">Sign Up</h2>
+        <hr />
+        <div className="form-control ">
           <label className="label">
-            <span className="label-text">Email</span>
+            <span className="label-text text-white">Email</span>
           </label>
           <input
           name="email"
             type="email"
             placeholder="email"
-            className="input input-bordered"
+            className="input bg-slate-300 rounded-full input-bordered"
             required
           />
         </div>
         <div className="form-control">
           <label className="label">
-            <span className="label-text">Password</span>
+            <span className="label-text text-white ">Password</span>
           </label>
           <input
           name="password"
             type="password"
             placeholder="password"
-            className="input input-bordered"
+            className="input bg-slate-300 rounded-full input-bordered"
             required
           />
           <div className="flex justify-around">
             <label className="label">
-              <a href="#" className="label-text-alt link link-hover">
+              <a href="#" className="label-text-alt text-white  link link-hover">
                 Forgot password?
               </a>
             </label>
             <label className="label">
-              <a href="#" className="label-text-alt link link-hover">
-                <input type="checkbox" name="" id="" /> Remember Me
+              <a href="#" className="label-text-alt text-white link link-hover">
+                <input  type="checkbox" name="" id="" /> Remember Me
               </a>
             </label>
           </div>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn bg-slate-400">Login</button>
         </div>
       </form>
       {
