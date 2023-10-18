b928e945a1480dcb8927b90b500a1ffee11d473f
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-10-18 08:13:44 +0600

    Update setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h2 className='text-3xl text-center'>Welcome to the future of the Technology</h2>
+    </>
+  )
+}
+
+export default App
