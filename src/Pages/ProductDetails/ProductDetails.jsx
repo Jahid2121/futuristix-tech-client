0a9578619d0ce9c38a958dc680d4444cb11aba92
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-10-19 12:23:40 +0600

    Send product data to the server when add to cart button clicked

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Navbar from "../../Shared/Navbar";
+import Swal from "sweetalert2";
 
 const ProductDetails = () => {
     const product = useLoaderData();
     const {_id, productName, price, brandName, description, image, ratings, type } =
     product;
-  console.log(product);
+
+    const handleAddToCart = product => {
+      fetch('http://localhost:5000/cartProducts', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(product)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if(data.insertedId){
+          Swal.fire({
+            title: 'Success!',
+            text: 'Product added Successfully',
+            icon: 'success',
+          })
+        }
+      })
+    }
   return (
     <>
     <Navbar></Navbar>
@@ -23,7 +44,9 @@ const ProductDetails = () => {
         <p className="text-xl  font-medium">${price}</p>
         <p>{description}</p>
         <div className="card-actions justify-start">
-          <button className="btn bg-blue-600 text-white">Add to Cart</button>
+          <button
+          onClick={() => handleAddToCart(product)}
+          className="btn bg-blue-600 text-white">Add to Cart</button>
         </div>
       </div>
     </div>
