fb7db09bc8b44ad70286135e0b7f3103ae37fd31
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-10-21 23:13:05 +0600

    Update Navigation to previous and next in slider

--- src/Shared/Banner.jsx
+++ src/Shared/Banner.jsx
@@ -6,8 +6,8 @@ const Banner = () => {
         <div  className=''>
             <img className='w-[390px]  md:w-full mx-auto' src={bannerImg} alt="" />
             <div className='md:-mt-[600px] -mt-40 mb-20 md:mb-72 w-40 text-center'>
-            <h2 className=' font-bold  text-sm md:text-5xl w-40 ml-20 md:w-full md:mb-4 text-[#F1EFEF]'>Shop the Latest Gadgets, <br /> Explore Limitless Possibilities!</h2>
-            <Link to={'/Add Product'}><button className='relative ml-28 mt-3 btn bg-blue-600 border-0 text-white'>Add Product</button></Link>
+            <h2 className=' font-bold  text-sm md:text-5xl w-40 ml-80 flex  md:w-full md:mb-4 text-[#F1EFEF]'>Shop the Latest Gadgets, <br /> Explore Limitless Possibilities!</h2>
+            <Link to={'/Add Product'}><button className='relative md:ml-[420px] mt-3 btn bg-blue-600 border-0 text-white'>Add Product</button></Link>
             </div>
         </div>
     );
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,29 +1,47 @@
 import React from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 // Import Swiper styles
 import 'swiper/css';
-
 
 
 const Slider = ({ findProducts}) => {
   const products = findProducts?.find(product => product)
   console.log(products);
     return (
-        <Swiper 
+        <Swiper  className='static'
       spaceBetween={1}
-      slidesPerView={2.5}
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      navigation={{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }}
+      slidesPerView={2}
       // onSlideChange={() => console.log('slide change')}
       // onSwiper={(swiper) => console.log(swiper)}
     >
       {
         products?.image.map((img,idx) => <SwiperSlide  key={idx}><img className='w-80 h-64' src={img} alt="" /></SwiperSlide>)
+        
       }
-
+      <div className="relative bg-green-900">
+        <div className="swiper-button-next">Next</div>
+        <div className="swiper-button-prev">Prev</div>
+        <div className="-mt-20    relative ">
+        <div style={{}} className="swiper-button-next  mr-96"></div>
+        <div className="swiper-button-prev"></div>
+        </div>
+        </div>
+        <div className="-mt-20    relative ">
+        <div style={{}} className="swiper-button-next "></div>
+        <div className="swiper-button-prev"></div>
+        </div>
       
      
      <p className='text-center'> ...</p>
+
+     
     </Swiper>
     );
 };
