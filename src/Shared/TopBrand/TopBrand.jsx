83a13e7469e9298d3591961909aa014cfe70704b
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-03 08:11:21 +0600

    Add selection style

--- src/Shared/TopBrand/TopBrand.jsx
+++ src/Shared/TopBrand/TopBrand.jsx
@@ -4,8 +4,8 @@ import Brand from "./Brand";
 const TopBrand = ({products}) => {
   return (
     <>
-    <div data-aos="fade-up" className="text-center">
-      <h2 className="md:text-3xl text-2xl font-bold mb-7">Explore Top Brands</h2>
+    <div data-aos="fade-up" className="text-center mt-32 ">
+      <h2 className="md:text-5xl text-2xl font-bold mb-7">Explore Top Brands</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
         {
